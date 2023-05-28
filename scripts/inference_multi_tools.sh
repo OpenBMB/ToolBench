724fea0e9962f22c2cece5a8ac5b14449dc27082
pooruss <shihaoliang0828@163.com>
2023-05-28 19:29:37 +0800

    update scripts

--- scripts/inference_multi_tools.sh
+++ scripts/inference_multi_tools.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 source BMTools/secret_keys.sh
 export PYTHONPATH=BMTools
-python toolbench/inference/inference_multi_tools.py --model_path /path/to/tool-llama/weights
+python toolbench/inference/inference_multi_tools.py \
+    --model_path /path/to/tool-llama/weights
