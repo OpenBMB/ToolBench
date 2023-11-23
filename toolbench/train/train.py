f13f9c8f5ca9b1845eb822f77f8ccb9833273208
yhyu13 <yohan680919@gmail.com>
2023-11-23 11:46:21 +0800

    Use QLoRA along side flash attn2

--- toolbench/train/train.py
+++ toolbench/train/train.py
@@ -273,11 +273,18 @@ def train():
     world_size = int(os.environ.get("WORLD_SIZE", 1))
     ddp = world_size != 1
     device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)} if ddp else None
+    bnb_config = transformers.BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
         device_map=device_map,
         use_flash_attention_2=True,
+        quantization_config=bnb_config,
     )
     model.config.use_cache = False
     trainer = Trainer(
