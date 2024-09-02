1a66ffa6b97aaadd36d27be9f29b8adc9dd81ee7
Wei <21039366+Mai0313@users.noreply.github.com>
2024-09-02 07:23:15 +0000

    chore: Refactor code to simplify directory creation in rapidapi.py

--- toolbench/inference/Downstream_tasks/rapidapi.py
+++ toolbench/inference/Downstream_tasks/rapidapi.py
@@ -487,9 +487,7 @@ class pipeline_runner:
         if callbacks is None:
             if server: print("Warning: no callbacks are defined for server mode")
             callbacks = []
-        splits = output_dir_path.split("/")
-        os.makedirs("/".join(splits[:-1]),exist_ok=True)
-        os.makedirs("/".join(splits),exist_ok=True)
+        os.makedirs(output_dir_path ,exist_ok=True)
         output_file_path = os.path.join(output_dir_path,f"{query_id}_{method}.json")
         if (not server) and os.path.exists(output_file_path):
             return
